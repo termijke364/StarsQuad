290bb147f30baaef41f839bb06d53f2f3ecd4871
termijke364 <122256312+termijke364@users.noreply.github.com>
2023-01-09 11:14:03 +0400

    Add files via upload

---
+++ StarsQuad.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int InputSide()
+{
+	int side;
+	cin >> side;
+
+	return side;
+}
+
+void Stars(int length, int height)
+{
+	for (int r = 0; r < length; r++)
+	{
+		for (int c = 0; c < height; c++)
+		{
+			if (r == 0 || c == 0 || r == length - 1 || c == height - 1)
+				cout << "*";
+			else
+				cout << " ";
+		}
+		cout << endl;
+	}
+}
+
+int main()
+{
+	setlocale(0, "");
+
+	int length{}, height{};
+
+	cout << "Введите ширину четырехугольника: " << endl;
+	height = InputSide();
+
+	cout << "Введите высоту четырехугольника: " << endl;
+	length = InputSide();
+
+	Stars(height, length);
+
+	return 0;
+}
